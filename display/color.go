e1d58d5b80dcd50607ef318be87a4827fb2ee336
Janez Kenda <janez.kenda@gmail.com>
2020-05-12 17:55:22 +0200

    Implemented a function for generating gradients.

--- display/color.go
+++ display/color.go
@@ -1,6 +1,20 @@
 package display
 
+import "image/color"
+
 type Color uint16
+
+func (c Color) ToRGB() color.RGBA {
+	return color.RGBA{
+		R: uint8(c & 0x1F),
+		G: uint8(c >> 5 & 0x1F),
+		B: uint8(c >> 10 & 0x1F),
+	}
+}
+
+func ColorFromRGB(c color.RGBA) Color {
+	return Color(c.B)<<10 | Color(c.G)<<5 | Color(c.R)
+}
 
 const (
 	ClrBlack  Color = 0x0000
@@ -15,4 +29,24 @@ const (
 
 func RGB15(red, green, blue uint32) Color {
 	return Color(red | green<<5 | blue<<10)
+}
+
+func Gradient(min, max Color, size int) []Color {
+	minC := min.ToRGB()
+	maxC := max.ToRGB()
+
+	rDiff := int(maxC.R)-int(minC.R)
+	gDiff := int(maxC.G)-int(minC.G)
+	bDiff := int(maxC.B)-int(minC.B)
+
+	var gradient []Color
+	for i := 0; i < size; i++ {
+
+		gradient = append(gradient, ColorFromRGB(color.RGBA{
+			R: uint8(int(minC.R) + rDiff*i/size),
+			G: uint8(int(minC.G) + gDiff*i/size),
+			B: uint8(int(minC.B) + bDiff*i/size),
+		}))
+	}
+	return gradient
 }
